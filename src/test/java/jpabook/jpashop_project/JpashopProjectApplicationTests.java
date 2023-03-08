db93034895f25fa5d490d4da3e973ae239201a15
ggssww99 <ggssww99@gmail.com>
2023-03-08 21:48:30 +0900

    JPA Project_v1

---
+++ src/test/java/jpabook/jpashop_project/JpashopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
