fff8027d5d098127e23127f90024c030c191ff40
ggssww99 <ggssww99@gmail.com>
2023-03-11 21:42:15 +0900

    회원 서비스 개발

---
+++ src/main/java/jpabook/jpashop_project/service/MemberService.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop_project.service;
+
+import jpabook.jpashop_project.domain.Member;
+import jpabook.jpashop_project.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor  //final있는 필드만 생성자를 만들어 줌 .
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     *회원가입
+     */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
