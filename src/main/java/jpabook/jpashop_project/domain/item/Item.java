0adb1f3bd77e7a812a55b2213ccf4da9b5cfbf1d
ggssww99 <ggssww99@gmail.com>
2023-03-11 23:56:01 +0900

    상품 서비스 개발

--- src/main/java/jpabook/jpashop_project/domain/item/Item.java
+++ src/main/java/jpabook/jpashop_project/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop_project.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop_project.domain.Category;
+import jpabook.jpashop_project.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,4 +29,25 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //==비지니스  로직==//
+    /**
+     *
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     *
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+
+    }
 }
