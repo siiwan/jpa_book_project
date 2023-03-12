6f94fc37671cadbdd5c5e62b2a4df0e54088db4a
ggssww99 <ggssww99@gmail.com>
2023-03-12 10:53:19 +0900

    주문 검색 기능 개발

---
+++ src/main/java/jpabook/jpashop_project/repository/OrderSearch.java
@@ -0,0 +1,13 @@
+package jpabook.jpashop_project.repository;
+
+import jpabook.jpashop_project.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class OrderSearch {
+
+    private String memberName;
+    private OrderStatus orderStatus;
+
+}
