72abe6e01d95c902177d0f99d55d62c316c3a697
ggssww99 <ggssww99@gmail.com>
2023-03-12 15:07:51 +0900

    회원 등록 개발

---
+++ src/main/java/jpabook/jpashop_project/controller/MemberController.java
@@ -0,0 +1,44 @@
+package jpabook.jpashop_project.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop_project.domain.Address;
+import jpabook.jpashop_project.domain.Member;
+import jpabook.jpashop_project.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberFrom";
+
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "members/createMemberFrom";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
+}
